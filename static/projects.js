1c2f3fc66c3c89e2f9a051e81b23b4ab52ae9685
Theo Henson <theodorehenson@protonmail.com>
2020-06-05 00:56:11 -0400

    Add noscript message

--- static/projects.js
+++ static/projects.js
@@ -3,14 +3,15 @@ var projs = new Vue({
     data: {
 	projects: []
     }
-})
+});
 
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "/api/projects", true)
+xhr.open("GET", "/api/projects", true);
 xhr.onload = function (e) {
     if (xhr.readyState === 4) {
 	if (xhr.status === 200) {
-	    projs.projects = JSON.parse(xhr.responseText)
+	    document.getElementById("projs").style.display = "flex";
+	    projs.projects = JSON.parse(xhr.responseText);
 	}
     }
 }
