659f02183e36b954486c6abcad2f6025ad17d19c
Theo Henson <theodorehenson@protonmail.com>
2020-06-05 12:55:35 -0400

    Add loading sign

--- static/projects.js
+++ static/projects.js
@@ -1,3 +1,5 @@
+document.getElementById("loading").style.display = "block";
+
 var projs = new Vue({
     el: "#projs",
     data: {
@@ -10,6 +12,7 @@ xhr.open("GET", "/api/projects", true);
 xhr.onload = function (e) {
     if (xhr.readyState === 4) {
 	if (xhr.status === 200) {
+	    document.getElementById("loading").style.display = "none";
 	    document.getElementById("projs").style.display = "flex";
 	    projs.projects = JSON.parse(xhr.responseText);
 	}
